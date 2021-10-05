a86dbd1abcb6acd50647470ae8820e697df0997f
Tony Amirault <tonyamirault@MacBook-Pro-Tony.local>
2021-10-05 09:45:35 +0200

    added weather data values

---
+++ weather.data.ts
@@ -0,0 +1,294 @@
+const weatherData = [
+    {
+        "key": 1630879200000,
+        "RAIN_FALL": {},
+        "TEMPERATURE": {},
+        "docCount": 0
+    },
+    {
+        "key": 1630965600000,
+        "RAIN_FALL": {},
+        "TEMPERATURE": {},
+        "docCount": 0
+    },
+    {
+        "key": 1631052000000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 25.15757575757576
+        },
+        "docCount": 33
+    },
+    {
+        "key": 1631138400000,
+        "RAIN_FALL": {
+            "value": 5.588
+        },
+        "TEMPERATURE": {
+            "value": 21.263333333333335
+        },
+        "docCount": 98
+    },
+    {
+        "key": 1631224800000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 20.189791666666668
+        },
+        "docCount": 96
+    },
+    {
+        "key": 1631311200000,
+        "RAIN_FALL": {
+            "value": 1.016
+        },
+        "TEMPERATURE": {
+            "value": 18.180416666666666
+        },
+        "docCount": 98
+    },
+    {
+        "key": 1631397600000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 17.428541666666668
+        },
+        "docCount": 96
+    },
+    {
+        "key": 1631484000000,
+        "RAIN_FALL": {
+            "value": 0.508
+        },
+        "TEMPERATURE": {
+            "value": 17.670520833333335
+        },
+        "docCount": 97
+    },
+    {
+        "key": 1631570400000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 18.5471875
+        },
+        "docCount": 96
+    },
+    {
+        "key": 1631656800000,
+        "RAIN_FALL": {
+            "value": 0.508
+        },
+        "TEMPERATURE": {
+            "value": 18.428854166666667
+        },
+        "docCount": 97
+    },
+    {
+        "key": 1631743200000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 17.340208333333333
+        },
+        "docCount": 96
+    },
+    {
+        "key": 1631829600000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 17.526979166666667
+        },
+        "docCount": 96
+    },
+    {
+        "key": 1631916000000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 17.966979166666665
+        },
+        "docCount": 96
+    },
+    {
+        "key": 1632002400000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 18.11791666666667
+        },
+        "docCount": 96
+    },
+    {
+        "key": 1632088800000,
+        "RAIN_FALL": {
+            "value": 4.318
+        },
+        "TEMPERATURE": {
+            "value": 17.075625
+        },
+        "docCount": 97
+    },
+    {
+        "key": 1632175200000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 14.468333333333334
+        },
+        "docCount": 96
+    },
+    {
+        "key": 1632261600000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 14.499583333333334
+        },
+        "docCount": 96
+    },
+    {
+        "key": 1632348000000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 15.543437500000001
+        },
+        "docCount": 96
+    },
+    {
+        "key": 1632434400000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 17.074583333333333
+        },
+        "docCount": 96
+    },
+    {
+        "key": 1632520800000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 16.523229166666667
+        },
+        "docCount": 96
+    },
+    {
+        "key": 1632607200000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 18.241041666666668
+        },
+        "docCount": 96
+    },
+    {
+        "key": 1632693600000,
+        "RAIN_FALL": {
+            "value": 4.064
+        },
+        "TEMPERATURE": {
+            "value": 15.9559375
+        },
+        "docCount": 99
+    },
+    {
+        "key": 1632780000000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 14.295000000000002
+        },
+        "docCount": 96
+    },
+    {
+        "key": 1632866400000,
+        "RAIN_FALL": {
+            "value": 4.572
+        },
+        "TEMPERATURE": {
+            "value": 13.23375
+        },
+        "docCount": 98
+    },
+    {
+        "key": 1632952800000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 11.699062500000002
+        },
+        "docCount": 96
+    },
+    {
+        "key": 1633039200000,
+        "RAIN_FALL": {
+            "value": 5.334
+        },
+        "TEMPERATURE": {
+            "value": 13.625833333333333
+        },
+        "docCount": 101
+    },
+    {
+        "key": 1633125600000,
+        "RAIN_FALL": {
+            "value": 6.8580000000000005
+        },
+        "TEMPERATURE": {
+            "value": 13.621666666666668
+        },
+        "docCount": 100
+    },
+    {
+        "key": 1633212000000,
+        "RAIN_FALL": {
+            "value": 26.162
+        },
+        "TEMPERATURE": {
+            "value": 13.175625000000002
+        },
+        "docCount": 100
+    },
+    {
+        "key": 1633298400000,
+        "RAIN_FALL": {
+            "value": 1.524
+        },
+        "TEMPERATURE": {
+            "value": 13.230208333333335
+        },
+        "docCount": 97
+    },
+    {
+        "key": 1633384800000,
+        "RAIN_FALL": {
+            "value": 0
+        },
+        "TEMPERATURE": {
+            "value": 12.09842105263158
+        },
+        "docCount": 38
+    }
+] as const
